77ef49236bb1b04e17ec79c5387210ef7e20d7a2
mansi59054 <48704613+mansi59054@users.noreply.github.com>
2020-09-21 14:26:41 +0530

    Update Main.java
    Check for last digit

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ import java.util.*;
 import java.io.*;
 public class Main
 {
+	
+public static int lastdigit(int n){				//Function that finds the last digit of the number
+    return n%10;
+}
+	
 public static void function1(int n){				// Function to check if the number is divisible by 3, 5 or both
 	for(int i=1;i<=n;i++){
 		boolean divby3 = (i%3==0);
@@ -10,10 +15,10 @@ public static void function1(int n){				// Function to check if the number is di
 		if(divby3 && divby5){
 			System.out.println("FizzBuzz");
 		}
-		else if(divby3){
+		else if(divby3 || lastdigit(i)==3){		//Add one more condition in if
 			System.out.println("Fizz");
 		}
-		else if(divby5){
+		else if(divby5 || lastdigit(i)==5){
 			System.out.println("Buzz");
 		}
 		else{
